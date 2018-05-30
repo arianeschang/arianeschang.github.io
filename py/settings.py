7881cdb915d5f54f7ea15b4f70bb5a5767a89a1d
Ariane Schang <arianeschang@gmail.com>
2018-05-30 17:29:36 -0400

    adding files

---
+++ py/settings.py
@@ -0,0 +1,12 @@
+# packages: twython, NLTK, NLTK Vader, textblob
+
+API_KEY = "LZSjvGnEOdS9BohmAHRXs1t6x"
+API_SECRET = "u76eifr4XlX8hvdLe6IMfWjZqPAsDOrR76Wx96syQcbnwo0rb8"
+ACCESS_TOKEN = "AAAAAAAAAAAAAAAAAAAAADha6QAAAAAAqWR1KQPjt6RS3tcLePTD%2BFleuis%3DIPLsKNwUV3loaFURNKxRN0zRYZDRo7GZ4lwIrvYxPOgmFRp85D"
+
+QUERY = ["#kindermorgan", "kindermorgan", "kinder morgan"]
+
+TWEET_COUNT_PER_DAY = 100
+
+OUTPUT_CSV = "kindermorgan.csv"
+NAME_OF_CHART = "Kinder Morgan Sentiment"
